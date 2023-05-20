9bffffbb44b8be79cc56760f3a67bc8d737a0000
KanameMadoka <brian871212@gmail.com>
2023-05-20 11:43:15 +0800

    程式小修改

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import {useNavigate} from "react-router-dom";
 import Axios from "axios";
 import {HashTable} from "./Componets/test";
 import HashTableComponent from "./Componets/test";
-import { HasakiTable } from "./Componets/test2";
-
-
+// import { HasakiTable } from "./Componets/test2";
 function App() {
   const [name, setName] = useState("");
   const [account, setAccount] = useState("");
@@ -15,48 +13,32 @@ function App() {
   const [employeeList, setEmployeeList] = useState([]);
   const [count,setCount] =useState([]);
   let navigate =useNavigate();
-
-  const hsakitable = new HasakiTable(5)
-  hsakitable.set('Kaname','Madoka')
-  hsakitable.set('Akemi','Houra')
-  hsakitable.set('sakura','kyoko')
-  hsakitable.set('miki','sayaka')
-  hsakitable.set('tomoe','mami')
-  console.log('我是hsakitbale的keys',hsakitable.Keys())
-  console.log('我是hsakitable的values',hsakitable.values())
-  console.log('======================')
-  console.log(hsakitable.get('Kaname'))
-  console.log('======================')
-  console.log(hsakitable.get('miki'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // const hsakitable = new HasakiTable(5)
+  // hsakitable.set('Kaname','Madoka')
+  // hsakitable.set('Akemi','Houra')
+  // hsakitable.set('sakura','kyoko')
+  // hsakitable.set('miki','sayaka')
+  // hsakitable.set('tomoe','mami')
+  // console.log('我是hsakitbale的keys',hsakitable.Keys())
+  // console.log('我是hsakitable的values',hsakitable.values())
+  // console.log('======================')
+  // console.log(hsakitable.get('Kaname'))
+  // console.log('======================')
+  // console.log(hsakitable.get('miki'))
   const handleIncrement = () => {
     setCount(prevCount => prevCount + 1);    
     // 在使用 setter 函數更新狀態值時，React 會自動傳遞當前的狀態值給這個函數。而在 setCount(prevCount => prevCount + 1) 中，prevCount 就是這個傳遞進來的當前狀態值。
     // 所以你可以隨意改變數名
   }
     //測試哈希表的功能
-  const hashadd = () =>{
-    const hashTable = new HashTable()
-    hashTable.add(count,count+1)
-    console.log('以下是你輸入的哈希表')
-    console.log('我是hashTable的keys',hashTable.keys())
-    console.log('我是hashTable的Values',hashTable.values())
-    handleIncrement()
-  }
+  // const hashadd = () =>{
+  //   const hashTable = new HashTable()
+  //   hashTable.add(count,count+1)
+  //   console.log('以下是你輸入的哈希表')
+  //   console.log('我是hashTable的keys',hashTable.keys())
+  //   console.log('我是hashTable的Values',hashTable.values())
+  //   handleIncrement()
+  // }
   //測試挑轉功能
   function jump(){
     //測試已OK 真的可以跳轉
@@ -67,15 +49,15 @@ function App() {
     Axios.post("http://localhost:3001/login",{
     account: account,
     password:password,}).then((response)=>{
-      if(response){
+      if(response.data){
+        console.log(response.data);
         navigate("/main");
       }else{
+        console.log(response.data);
         console.log("帳號密碼錯誤");
       }
     });
-  };
-  
-  
+  };  
   const addEmployee = () => {
     Axios.post("http://localhost:3001/create", {
       name: name,
@@ -92,13 +74,11 @@ function App() {
       ]);
     });
   };
-
   const getEmployees = () => {
     Axios.get("http://localhost:3001/employees").then((response) => {
       setEmployeeList(response.data);
     });
   };
-
   const updateEmployeePassword = (id) => {
     Axios.put("http://localhost:3001/update", { password: newpassword, id: id }).then(
       (response) => {
@@ -117,7 +97,6 @@ function App() {
       }
     );
   };
-
   const deleteEmployee = (id) => {
     Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
       setEmployeeList(
@@ -127,9 +106,6 @@ function App() {
       );
     });
   };  
-
-
-
   return (
     <div className="App">
       <div className="information">
@@ -158,9 +134,9 @@ function App() {
         <button onClick={login}>login</button>
         <button onClick={jump}>mainpage</button>
         <button onClick={addEmployee}>register</button>
-        <button onClick={hashadd}>哈希表測試</button>
-        <h1>My App</h1>
-        <HashTableComponent />
+        {/* <button onClick={hashadd}>哈希表測試</button> */}
+        {/* <h1>My App</h1>
+        <HashTableComponent /> */}
       </div>
       <div className="employees">
         <button onClick={getEmployees}>Show Employees</button>
@@ -189,7 +165,6 @@ function App() {
                   {" "}
                   Update
                 </button>
-
                 <button
                   onClick={() => {
                     deleteEmployee(val.id);
@@ -205,5 +180,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
